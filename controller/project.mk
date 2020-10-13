da98a5b1dbc5cb7d0ee33d308f0b105e450369e6
Pavel Repin <pavel99repin@mail.ru>
2020-10-13 11:23:57 +0300

    new mk

--- controller/project.mk
+++ controller/project.mk
@@ -1,9 +1,8 @@
-PROJECT_TESTS   = tests/test_tmp_control.c     \
+PROJECT_MODULES = src/tmp_control.c  \
+
+PROJECT_TESTS   = tests/test_tmp_control.c \
 # C source files
-PROJECT_CSRC 	= src/main.c          \
-                  src/tmp_control.c   \
-                  $(PROJECT_TESTS)
-  
+PROJECT_CSRC 	= src/main.c $(PROJECT_MODULES) $(PROJECT_TESTS) 
 # C++ source files
 PROJECT_CPPSRC 	= 
 # Directories to search headers in
