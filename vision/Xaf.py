c43a2b8cac969dd928f382cf0fef7b4d310f38d5
Pavel Repin <pavel99repin@mail.ru>
2021-05-12 23:36:07 +0300

    new_cv

--- vision/Xaf.py
+++ vision/Xaf.py
@@ -8,10 +8,10 @@ from SearchCenterMarks import SearchMarks
 if __name__ == "__main__":
     log = logging.getLogger(__name__)
     dirname=(os.path.dirname(inspect.getfile(inspect.currentframe())))
-    name_1 = 'video\EV_zawrka_2_left_line.mp4'
-    name_2 = 'video\EV_zawrka_1_right_line.mp4'
-    name_3 = 'video\EV_zawrka_3_center.mp4'
-    cap = cv2.VideoCapture(os.path.join(dirname,name_3))
+    name_1 = 'EV_zawrka_2_left_line.mp4'
+    name_2 = 'EV_zawrka_1_right_line.mp4'
+    name_3 = 'EV_zawrka_3_center.mp4'
+    cap = cv2.VideoCapture(os.path.join(dirname,'video',name_1))
     if not cap.isOpened():
         log.warning("Video doesn't open")
         exit()
